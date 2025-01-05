dfc834f63a3cecaec465ec44adc887a33d2021d1
Jihyeok <bluesaurus2@gmail.com>
2025-01-05 10:58:54 -0800

    Adding iOS project

---
+++ AlternativeFuelStation_iOS/AlternativeFuelStation_iOS/AlternativeFuelStation_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlternativeFuelStation_iOSApp.swift
+//  AlternativeFuelStation_iOS
+//
+//  Created by Choi Jihyeok on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct AlternativeFuelStation_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
